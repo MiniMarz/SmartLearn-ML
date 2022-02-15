b6aedba45a5ac8ffdc620efcf87c81e2dafce2b7
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2022-02-15 14:50:27 +0000

    Fix test compilation

--- clustering/Cluster.java
+++ clustering/Cluster.java
@@ -139,3 +139,62 @@ public class Cluster
   }
 
   /**
+   * Make the distance array of pairwise distances in diagonal form.
+  **/
+  public static void setDistances(String filename)
+  {
+    try
+    {
+      BufferedReader bf=new BufferedReader(new FileReader(filename));
+      // read the first line that contains the number of points
+      String newLine = bf.readLine();
+      StringTokenizer st = new StringTokenizer(newLine);
+      distances=new double[Integer.parseInt(st.nextToken())][];
+      int numPoints;
+      // for each point
+      for (int i=0; i < distances.length; i++)
+      {
+        newLine=bf.readLine();
+        st=new StringTokenizer(newLine);
+        // ...read its distance from other points
+        numPoints=st.countTokens()-1;
+        double[] tempDist=new double[numPoints];
+        st.nextToken();
+        for (int j=0; j < numPoints; j++)
+        {
+          tempDist[j]=Double.parseDouble(st.nextToken());
+        }
+        distances[i]=tempDist;
+      }
+    }
+    catch (IOException e)
+    {
+      System.err.println(e.toString());
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Returns the distance between point i and j.
+  **/
+  private static double dist(int i, int j)
+  {
+    if (i<j)
+      return distances[i][j-i-1];
+    else if(i==j)
+      return 0;
+    else
+      return distances[j][i-j-1];
+  }
+
+  /**
+   * Scale the data set of distances.
+   * New value=(value-min)/(max-min).
+  **/
+  public static void scaleDistances()
+  {
+    // looking for the max and min...
+    double current=distances[0][0];
+    double max=0, min=0;
+    for (int i=0;i<distances.length;i++)
+    {
