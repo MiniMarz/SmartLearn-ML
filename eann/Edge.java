3a9a9c31431ae42035d8b33040537351a2dea4f0
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2022-03-20 15:44:47 +0000

    jshinting

--- eann/Edge.java
+++ eann/Edge.java
@@ -14,4 +14,5 @@ public class Edge implements Serializable
   private double weight;            // the weight associated with this edge
   private Neuron source;            // the neuron it is connected to in layer i
   private Neuron dest;	            // the neuron it is connected to in layer i+1
-  private int id;	            // identif
+  private int id;	            // identifier for the edge
+  private int mode;	            // the mode to use f
