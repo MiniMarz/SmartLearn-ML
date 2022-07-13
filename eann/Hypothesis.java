3f00632c415d2b058354fb1d388923b50baa9aac
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2022-07-13 06:53:54 +0000

    drm/radeon: force dma32 to fix regression rs4xx,rs6xx,rs740

--- eann/Hypothesis.java
+++ eann/Hypothesis.java
@@ -1,2 +1,5 @@
 /**
- * This abstract class describes the behavior of an ANNHypothesis for
+ * This abstract class describes the behavior of an ANNHypothesis for use by the genetic algorithm
+  * Author: Alex Ksikes
+*/
+public abstr
