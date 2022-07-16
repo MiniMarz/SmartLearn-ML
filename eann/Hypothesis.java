c80e53358c2c92121b42600c55c3ab97fb32593f
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2022-07-16 06:36:02 +0000

    Cosmetic.

--- eann/Hypothesis.java
+++ eann/Hypothesis.java
@@ -2,4 +2,6 @@
  * This abstract class describes the behavior of an ANNHypothesis for use by the genetic algorithm
   * Author: Alex Ksikes
 */
-public abstr
+public abstract class Hypothesis
+{
+  /**************** Variables **********
