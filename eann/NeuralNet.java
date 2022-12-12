a0ca0bd7ce19bd91b168824e85c2a6608f2c0f3c
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2022-12-12 14:03:10 +0000

    rubocop upgrade and cleanup

---
+++ eann/NeuralNet.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+import java.io.*;
+import java.util.zip.*;
+
+/**
+  * NeuralNet Class : A neural network consists of edges and neuron
+  * Author: Alex Ksikes
+**/
+public class NeuralNet implements Serializable
+{
