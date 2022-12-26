b8e5d6039bd2ca6393afbeb275a30fc40d49a7b7
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2022-12-26 11:35:12 +0000

    Moved file.

--- eann/varyANNHypothesis.java
+++ eann/varyANNHypothesis.java
@@ -15,3 +15,63 @@ public class varyANNHypothesis extends Hypothesis
 
   // Each coordinate i represents the weights of the neuron in position i in the hidden layer
   // The weights of each neuron i are represented as a 2d array
+  // The first coordinate of the array represents the weights (from left to right)
+  // of the backward edges, the second coordinate represents the forward edges (from left to right)
+  private double[][][] representation;
+
+  // Number of hidden neurons
+  int numHiddenNeurons;
+
+  // The fitness of this hypothesis is the RMS of the corressponding neural net
+  double fitness;
+
+  // Training accuracy      // may be removed
+  double trainingAcc;
+  double evalAcc;
+
+  /**************** Methods **********************/
+
+  // Create an hypothesis of size 3 by default
+  public varyANNHypothesis()
+  {
+    this.numHiddenNeurons=3;
+    representation=new double[numHiddenNeurons][][];
+    for (int i=0;i<numHiddenNeurons;i++)
+    {
+      representation[i]=new double[numInputNeurons][numOutputNeurons];
+    }
+  }
+
+  // Constructor
+  public varyANNHypothesis(int numHiddenNeurons)
+  {
+    this.numHiddenNeurons=numHiddenNeurons;
+    representation=new double[numHiddenNeurons][][];
+    for (int i=0;i<numHiddenNeurons;i++)
+    {
+      representation[i]=new double[numInputNeurons][numOutputNeurons];
+    }
+  }
+
+  // Give to this hypothesis random representation
+  // Every weight is initialized to a random value between -1 and 1
+  public void setToRandom()
+  {
+    double[][] weights;
+    for (int index=0;index<numHiddenNeurons;index++)
+    {
+      weights=getWeights(index);
+      for(int i=0;i<numInputNeurons;i++)
+      {
+        weights[i][0]=Math.random()*2 - 1;
+      }
+      for (int i=0;i<numOutputNeurons;i++)
+      {
+        weights[0][i]=Math.random()*2 - 1;
+      }
+    }
+  }
+
+  // Compute fitness for this Hypothesis
+  // The fitness represents 3 digits of the RMS of this hypothesis
+  // So that we can keep the number of hidden neurons small by promoting smaller hypotheses
