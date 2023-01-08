d3a9516cc76f0aaf4268936f8e377acf2308c1b4
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-01-08 07:37:51 +0000

    portfolio updated via TUNE

--- ga/HypothesisComparator.java
+++ ga/HypothesisComparator.java
@@ -7,4 +7,6 @@ public class HypothesisComparator implements Comparator
   public HypothesisComparator() {}
 
   // Compare two hypotheses based on their fitness value
-  public int c
+  public int compare(Object o1,Object o2)
+  {
+    Hypothesis h1=(Hypothesi
