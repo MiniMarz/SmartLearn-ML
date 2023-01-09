8f84321d30b548cc21b0a3b051c5c019d1eecb2c
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-01-09 12:12:49 +0000

    add all file

--- gibbs/PatternFinder.java
+++ gibbs/PatternFinder.java
@@ -43,3 +43,73 @@ public class PatternFinder
   private double bestLikelihood;    // best likelihood of the patterns
   private int noUpdate;             // heuristic for convergence
   private int totalLength;          // length of all sequences
+  private double m;                 // speudo-count (number of virtual examples)
+
+  /**
+   * Constructor
+   *
+   * @param dataFile The file containing the sequences.
+   * @param L The length of the pattern.
+   * @param alphabet 0 for english alphabet, 1 for DNA.
+  **/
+  public PatternFinder(String dataFile, int L, int alphabet)
+  {
+    // select the alphabet
+    switch (alphabet)
+    {
+      case 0:
+        this.symbol=ENGLISH;
+        break;
+      case 1:
+        this.symbol=DNA;
+        break;
+      default:
+        System.exit(0);
+    }
+    this.numSymbols=symbol.length;
+    // read each sequence from the datafile
+    try
+    {
+      BufferedReader bf=new BufferedReader(new FileReader(dataFile));
+      int size=0;
+      while (bf.readLine()!=null)
+        size++;
+      this.N=size;
+      this.sequence=new String[N];
+      this.totalLength=0;
+      bf=new BufferedReader(new FileReader(dataFile));
+      for (int k=0; k<N; k++)
+      {
+        this.sequence[k]=bf.readLine();
+        this.totalLength+=sequence[k].length();
+      }
+    }
+    catch (IOException e)
+    {
+      System.err.println(e.toString());
+      e.printStackTrace();
+    }
+    // all other initializations
+    this.L=L;
+    this.q=new double[L][numSymbols];
+    this.p=new double[numSymbols];
+    this.a=new int[N];
+    this.besta=new int[N];
+    this.m=Math.sqrt(N);       // dependant on the number of sequences.
+  }
+
+  /**
+   * Runs the Gibbs sampling algorithm
+   *
+   * @param mode 1 for model refinement.
+  **/
+  public void run(int mode)
+  {
+    // Initialize the patterns by selecting random starting points for the pattern in each sequence
+    init();
+
+    // Iterate until the model converges or until a max number of iterations
+    int iteration=0, Z;
+    double [] A;
+    while(!converged() && iteration<=NUM_ITER)
+    {
