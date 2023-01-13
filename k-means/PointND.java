df528a819cbcf5eb6ccfab2c4d1394e04ee6c7a8
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-01-13 18:32:44 +0000

    daily backup at company

--- k-means/PointND.java
+++ k-means/PointND.java
@@ -104,3 +104,77 @@ public class PointND
     double max=coordinates[0];
     for (int i=1;i<dimension;i++)
     {
+      value=coordinates[i];
+      if (value > max)
+        max=value;
+    }
+    return max;
+  }
+
+ /**
+  * Return the probability of this point given it is normally distributed with a
+  * a diagonal covariance matrix of coefficients sigma
+ **/
+ public double normal(PointND mean,PointND sigma)
+  {
+    double mahalanobis;
+    double productSigma=1;
+    PointND temp=new PointND(this);
+    temp.subtract(mean);
+    // compute the product of the deviations and the mahalanobis distance
+    for (int i=0;i<dimension;i++)
+    {
+      productSigma=sigma.getCoordinate(i) * productSigma;
+      temp.setCoordinate( i, temp.getCoordinate(i)/sigma.getCoordinate(i) );
+    }
+    mahalanobis=Math.pow(temp.norm(),2);
+    return ( 1.0/( Math.pow((2*Math.PI),dimension/2.0) * productSigma )
+            * Math.exp(-1.0/2 * mahalanobis) );
+  }
+
+  /**
+   * Return coordinate i of this point
+  **/
+  public double getCoordinate(int i)
+  {
+    return coordinates[i];
+  }
+
+  /**
+   * Set coordinate[i] to the specified value
+  **/
+  public void setCoordinate(int i,double value)
+  {
+    coordinates[i]=value;
+  }
+
+  /**
+   * Return the number of coordinates of this point
+  **/
+  public int getDimension()
+  {
+    return dimension;
+  }
+
+  /**
+   * Return the coordinate array of this point
+  **/
+  public double[] getCoordinates()
+  {
+    return coordinates;
+  }
+
+  /**
+   * Return a string representation of this point
+  **/
+  public String toString()
+  {
+    String s="" + coordinates[0];
+    for (int i=1;i<dimension;i++)
+      s=s + " " + coordinates[i];
+    return s;
+  }
+
+
+
+}
