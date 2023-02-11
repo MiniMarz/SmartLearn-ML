1f8be6667c9b6dd4795b3595048bb28e913d61a7
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-02-11 06:14:05 +0000

    libceph: embed ceph messenger structure in ceph_client

--- knn/kNNExample.java
+++ knn/kNNExample.java
@@ -12,4 +12,5 @@ public class kNNExample extends Example
 
   private double relativeDistance;         // The distance this example is from another example
   private double weight;                   // Weight given to this example
-  private int id;     
+  private int id;                          // A unique id for this example
+  private static do
