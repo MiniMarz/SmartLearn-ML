3668822244880c666941a5da31898ec132cf6c6e
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-03-05 15:19:25 +0000

    Bumping knative-prow

--- neural-net/Edge.java
+++ neural-net/Edge.java
@@ -13,4 +13,5 @@ public class Edge implements Serializable
 
   private double weight;            // the weight associated with this edge
   private Neuron source;            // the neuron it is connected to in layer i
-  private Neuron dest;	       
+  private Neuron dest;	            // the neuron it is connected to in layer i+1
+  private int id;	            // 
