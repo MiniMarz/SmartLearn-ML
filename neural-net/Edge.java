6af1481bb4c583327e6cfd00b397be0e8a21943e
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-03-23 22:10:57 +0000

    tracing/syscalls: Fix perf syscall tracing when syscall_nr == -1

--- neural-net/Edge.java
+++ neural-net/Edge.java
@@ -49,4 +49,5 @@ public class Edge implements Serializable
   /**
   * Resets the weight for the edge with some random value
   * Chooses the weights uniformly between w and -w
-  * Where w=1
+  * Where w=1/sqrt(n) and n=# of inputs going into the destination of this edge
+  *
