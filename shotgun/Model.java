1a027baa836ccf38cdf53bc16557562b219e4fca
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-11-10 06:00:41 +0000

    Refactorizacion

--- shotgun/Model.java
+++ shotgun/Model.java
@@ -18,4 +18,4 @@ public abstract class Model implements Comparable
 {
 
   protected static final int ACC=0, RMS=1, ROC=2, ALL=3 , BEP=4 , PRE=5 , REC=6,
-             
+                             FSC=7, APR=8, LFT=9, CST=10, NRM=11, MXE=1
