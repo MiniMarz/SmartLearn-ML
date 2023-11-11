775b998810537de1842c549ff210ea93308aec1f
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-11-11 11:16:17 +0000

    Update NewInterface.php

--- shotgun/Model.java
+++ shotgun/Model.java
@@ -19,4 +19,5 @@ public abstract class Model implements Comparable
 
   protected static final int ACC=0, RMS=1, ROC=2, ALL=3 , BEP=4 , PRE=5 , REC=6,
                              FSC=7, APR=8, LFT=9, CST=10, NRM=11, MXE=12, BSP=13;
-  protected static final String[] measure={"ACC","RMS",
+  protected static final String[] measure={"ACC","RMS","ROC","ALL","BEP","PRE","REC",
+                                           "FSC","A
