c5677446fcaa03a66548b79bd184e15d8584da28
MiniMarz <142024589+MiniMarz@users.noreply.github.com>
2023-12-31 06:17:30 +0000

    Whitespaces

--- shotgun/Model.java
+++ shotgun/Model.java
@@ -89,4 +89,5 @@ public abstract class Model implements Comparable
     {
       for (int i=0; i<numPerf; i++)
       {
-       
+        perf=compute(i);
+        System.out.println(msg+" "+numModels+" "+meas
